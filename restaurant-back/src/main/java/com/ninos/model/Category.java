ecdbf06813026465340e45c993264bfcd508c027
NinoosMoshi <47070548+NinoosMoshi@users.noreply.github.com>
2021-03-23 08:46:19 -0500

    add Entity annotations Closes #8

--- restaurant-back/src/main/java/com/ninos/model/Category.java
+++ restaurant-back/src/main/java/com/ninos/model/Category.java
@@ -4,12 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "category")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "category_name")
     private String CategoryName;
 
 
--- restaurant-back/src/main/java/com/ninos/model/Order.java
+++ restaurant-back/src/main/java/com/ninos/model/Order.java
@@ -4,17 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "order")
 public class Order {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "order_name")
     private String OrderName;
+
+    @Column(name = "price")
     private BigDecimal price;
+
+    @Column(name = "image")
     private String img;
+
+    @Column(name = "description")
     private String description;
 
 

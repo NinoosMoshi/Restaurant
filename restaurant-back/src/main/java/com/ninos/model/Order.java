48522d279d9d3ac974b01f587fd6c8f9d81c46a7
NinoosMoshi <47070548+NinoosMoshi@users.noreply.github.com>
2021-03-23 08:36:01 -0500

    add Data Lombok Closes #7

--- restaurant-back/src/main/java/com/ninos/model/Order.java
+++ restaurant-back/src/main/java/com/ninos/model/Order.java
@@ -1,7 +1,14 @@
 package com.ninos.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
 
     private Long id;

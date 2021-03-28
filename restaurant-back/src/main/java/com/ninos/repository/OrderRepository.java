03103879a3893fe990d5ff5b4c76c8567a496f2a
NinoosMoshi <47070548+NinoosMoshi@users.noreply.github.com>
2021-03-28 18:56:15 -0500

    create repository (category and order) Closes #13

---
+++ restaurant-back/src/main/java/com/ninos/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.ninos.repository;
+
+import com.ninos.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
+}

9f4ae5f7b42d40426c45cacad09e00e834e3b00b
NinoosMoshi <47070548+NinoosMoshi@users.noreply.github.com>
2021-03-27 21:43:11 -0500

    create and update time stamp Closes #11

--- restaurant-back/src/main/java/com/ninos/model/Category.java
+++ restaurant-back/src/main/java/com/ninos/model/Category.java
@@ -3,8 +3,11 @@ package com.ninos.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.Set;
 
 @Data
@@ -20,6 +23,17 @@ public class Category {
 
     @Column(name = "category_name")
     private String CategoryName;
+
+    @Column(name="date_create")
+    @CreationTimestamp          // it will take local time and put it in the database
+    private Date dateCreated;
+
+    @Column(name = "date_update")
+    @UpdateTimestamp            // when we update time and it will put automatically in the database
+    private Date dateUpdated;
+
+
+
 
     @OneToMany(mappedBy = "category")
     private Set<Order> orders;
--- restaurant-back/src/main/java/com/ninos/model/Order.java
+++ restaurant-back/src/main/java/com/ninos/model/Order.java
@@ -3,9 +3,12 @@ package com.ninos.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -29,6 +32,17 @@ public class Order {
 
     @Column(name = "description")
     private String description;
+
+
+    @Column(name="date_create")
+    @CreationTimestamp          // it will take local time and put it in the database
+    private Date dateCreated;
+
+    @Column(name = "date_update")
+    @UpdateTimestamp            // when we update time and it will put automatically in the database
+    private Date dateUpdated;
+
+
 
     @ManyToOne
     @JoinColumn(name = "category_id")

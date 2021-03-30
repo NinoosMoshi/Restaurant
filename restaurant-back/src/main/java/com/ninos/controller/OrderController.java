c2c784df40e5d98b7c3cc82910c24f46372172c5
NinoosMoshi <47070548+NinoosMoshi@users.noreply.github.com>
2021-03-29 19:07:41 -0500

    get all orders Closes #20

--- restaurant-back/src/main/java/com/ninos/controller/OrderController.java
+++ restaurant-back/src/main/java/com/ninos/controller/OrderController.java
@@ -1,10 +1,16 @@
 package com.ninos.controller;
 
+import com.ninos.model.Order;
 import com.ninos.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api")
 public class OrderController {
 
     private OrderService orderService;
@@ -13,4 +19,12 @@ public class OrderController {
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
+
+    @GetMapping("/allOrders")
+    public List<Order> getAllOrders(){
+        return orderService.allOrders();
+    }
+
+
+
 }
--- restaurant-back/src/main/java/com/ninos/service/OrderService.java
+++ restaurant-back/src/main/java/com/ninos/service/OrderService.java
@@ -1,8 +1,11 @@
 package com.ninos.service;
 
+import com.ninos.model.Order;
 import com.ninos.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderService {
@@ -13,4 +16,12 @@ public class OrderService {
     public OrderService(OrderRepository orderRepo) {
         this.orderRepo = orderRepo;
     }
+
+
+    public List<Order> allOrders(){
+        return orderRepo.findAll();
+    }
+
+
+
 }

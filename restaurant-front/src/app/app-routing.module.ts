bbcc9b3cbcd90053afa3299955c9be1c4371f4f4
NinoosMoshi <47070548+NinoosMoshi@users.noreply.github.com>
2021-03-31 21:26:01 -0500

    Angular Route Closes #33

--- restaurant-front/src/app/app-routing.module.ts
+++ restaurant-front/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { OrderItemsComponent } from './components/order-items/order-items.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: OrderItemsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

2763544faec2d3b20096b13c33471f1319d0d08c
NinoosMoshi <47070548+NinoosMoshi@users.noreply.github.com>
2021-03-31 21:47:38 -0500

    Angular handle Route Closes #34

--- restaurant-front/src/app/app-routing.module.ts
+++ restaurant-front/src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path:'', component: OrderItemsComponent}
+  {path:'category/:id', component: OrderItemsComponent},
+  {path:'category', component: OrderItemsComponent},
+  {path:'order', component: OrderItemsComponent},
+  {path:'',redirectTo:'/order',pathMatch:'full'},
+  {path:'**',redirectTo:'/order',pathMatch:'full'}
 ];
 
 @NgModule({

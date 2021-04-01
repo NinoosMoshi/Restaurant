4cc579cddaa53d1d6d4416d77171903b33bf34a9
NinoosMoshi <47070548+NinoosMoshi@users.noreply.github.com>
2021-03-31 21:59:58 -0500

    Find Orders By Category Id(repository and service) Closes #35

--- restaurant-back/src/main/java/com/ninos/repository/OrderRepository.java
+++ restaurant-back/src/main/java/com/ninos/repository/OrderRepository.java
@@ -3,5 +3,10 @@ package com.ninos.repository;
 import com.ninos.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order,Long> {
+
+    public List<Order> findByCategoryId(Long id);
+
 }

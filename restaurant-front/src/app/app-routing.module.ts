82923496f1bc3ef3f4978360057ebaccf6efad4c
NinoosMoshi <47070548+NinoosMoshi@users.noreply.github.com>
2021-04-03 23:27:12 -0500

    Angular create Route order by id Closes #56

--- restaurant-front/src/app/app-routing.module.ts
+++ restaurant-front/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
+import { OrderDetailsComponent } from './components/order-details/order-details.component';
 import { OrderItemsComponent } from './components/order-items/order-items.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  {path:'order/:id', component: OrderDetailsComponent},
   {path:'category/:id', component: OrderItemsComponent},
   {path:'category', component: OrderItemsComponent},
   {path:'order/:key', component: OrderItemsComponent},

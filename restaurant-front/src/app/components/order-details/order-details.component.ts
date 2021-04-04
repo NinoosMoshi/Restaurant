74fdbe9f8dc63fde6619adf89ebdb957bac12974
NinoosMoshi <47070548+NinoosMoshi@users.noreply.github.com>
2021-04-04 00:12:19 -0500

    Angular order-detail show order Closes #58

--- restaurant-front/src/app/components/order-details/order-details.component.ts
+++ restaurant-front/src/app/components/order-details/order-details.component.ts
@@ -1,4 +1,7 @@
+import { Order } from './../../model/order';
+import { OrderService } from 'src/app/service/order.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-order-details',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderDetailsComponent implements OnInit {
 
-  constructor() { }
+  order: Order=null;
+
+  constructor(private orderService: OrderService, private activeRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getOrder();
+  }
+
+  getOrder(){
+    let id = this.activeRoute.snapshot.paramMap.get('id');
+    this.orderService.getOrderById(id).subscribe(
+      data =>{
+        this.order = data
+      }
+    )
   }
 
 }
